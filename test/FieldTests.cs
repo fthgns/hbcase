eaed3853d2fdc6466204473fd059dd20965b752a
Fatih Gunes <Fatih.Gunes@sabancinet.com>
2020-11-04 01:34:40 +0300

    solutiion structure & unit tests

---
+++ test/FieldTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using hb.Business;
+using hb.Business.Types;
+using Xunit;
+
+namespace test
+{
+    public class FieldTests
+    {
+        [Fact]
+        public void HandleMoves_ShouldArgumentNullException_WhenVehiclesIsNullOrEmpty()
+        {
+            var field = new Field(new Boundry(5, 5));
+            var result = Record.Exception(() => field.HandleMoves());
+            Assert.NotNull(result);
+            var exception = Assert.IsType<ArgumentNullException>(result);
+            var actual = exception.ParamName;
+            const string expected = "Vehicles";
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void IsValid_ShouldAssertFalse_WhenAnyVehicleIsOutOfBoundry()
+        {
+            var field = new Field(new Boundry(5, 5));
+            field.Vehicles.Add(new Vehicle(new Position(6, 6, Enums.Direction.E), new MoveSet("")));
+
+            Assert.False(field.IsValid());
+        }
+    }
+}
---
+++ test/VehicleTests.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using hb.Business;
+using hb.Business.Types;
+using Xunit;
+
+namespace test
+{
+    public class VehicleTests
+    {
+        [Fact]
+        public void Vehicle_ShouldArgumentNullException_WhenMovesIsNull()
+        {
+            var result = Record.Exception(()=> new Vehicle(new Position(1, 1, Enums.Direction.E), null));
+        
+            Assert.NotNull(result);
+            var exception = Assert.IsType<ArgumentNullException>(result);
+            var actual = exception.ParamName;
+            const string expected = "moves";
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Vehicle_ShouldArgumentNullException_WhenPositionIsNull()
+        {
+            var result = Record.Exception(() => new Vehicle(null, new MoveSet(string.Empty)));
+
+            Assert.NotNull(result);
+            var exception = Assert.IsType<ArgumentNullException>(result);
+            var actual = exception.ParamName;
+            const string expected = "pos";
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void HandleMoves_ShouldArgumentNullException_WhenMovesIsNullOrEmpty()
+        {
+            var vehicle = new Vehicle(new Position(1, 1, Enums.Direction.E), new MoveSet(string.Empty));
+            var result = Record.Exception(() => vehicle.HandleMoves());
+            Assert.NotNull(result);
+            var exception = Assert.IsType<ArgumentNullException>(result);
+            var actual = exception.ParamName;
+            const string expected = "moves";
+            Assert.Equal(expected, actual);
+        }
+    }
+}
